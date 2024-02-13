b36add8db3ce65ed2992d5d36e6d31dd3672e663
jacobtoddmathes <thejacobm1@tamu.edu>
2024-02-12 20:33:26 -0600

    fixing deployment

--- rails_root/db/schema.rb
+++ rails_root/db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_02_13_012651) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
--- rails_root/db/seeds.rb
+++ rails_root/db/seeds.rb
@@ -7,3 +7,33 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# seed data for the survey_profiles table
+
+SurveyProfile.create!([
+                        { user_id: 1, first_name: 'John', last_name: 'Doe', campus_name: 'Campus 1',
+                          district_name: 'District 1' },
+                        { user_id: 2, first_name: 'Jane', last_name: 'Doe', campus_name: 'Campus 2',
+                          district_name: 'District 2' },
+                        { user_id: 3, first_name: 'Jim', last_name: 'Doe', campus_name: 'Campus 3',
+                          district_name: 'District 3' }
+                      ])
+
+# seed data for the survey_responses table
+
+SurveyResponse.create!([
+                         { user_id: 1, leads_by_example: 5, ability_to_juggle: 5, communicator: 5, lifelong_learner: 5, high_expectations: 5,
+                           cooperative: 5, empathetic: 5, people_oriented: 5 },
+                         { user_id: 2, leads_by_example: 4, ability_to_juggle: 4, communicator: 4, lifelong_learner: 4, high_expectations: 4,
+                           cooperative: 4, empathetic: 4, people_oriented: 4 },
+                         { user_id: 3, leads_by_example: 3, ability_to_juggle: 3, communicator: 3, lifelong_learner: 3, high_expectations: 3,
+                           cooperative: 3, empathetic: 3, people_oriented: 3 }
+                       ])
+
+# Path: csce606-ELRC-Synergistic-Leadership-Theory/rails_root/db/schema.rb
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new
