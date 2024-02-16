a9401464ef2cecb446803f2f6f7acef44205cbfa
Andres Santiago <andylsantiago20@gmail.com>
2024-02-16 12:47:52 -0600

    added data model design user story cucumber tests

--- rails_root/features/step_definitions/stepdefs.rb
+++ rails_root/features/step_definitions/stepdefs.rb
@@ -1,30 +1,16 @@
+# Analysis Result Presentation Steps
 Given('I have completed the survey with invalid inputs') do
-  @attributes ||= {
-    user_id: nil,
-    leads_by_example: nil,
-    ability_to_juggle: nil,
-    communicator: nil,
-    lifelong_learner: nil,
-    high_expectations: nil,
-    cooperative: nil,
-    empathetic: nil,
-    people_oriented: nil
-  }
+  @attributes = {}
+  SurveyResponse.column_names.each do |name|
+    @attributes[name] = nil if name != 'id' && name != 'created_at' && name != 'updated_at'
+  end
 end
 
 Given('I have completed the survey with valid inputs') do
-  @attributes ||=
-    {
-      user_id: 100,
-      leads_by_example: 1,
-      ability_to_juggle: 1,
-      communicator: 1,
-      lifelong_learner: 1,
-      high_expectations: 1,
-      cooperative: 1,
-      empathetic: 1,
-      people_oriented: 1
-    }
+  @attributes = {}
+  SurveyResponse.column_names.each do |name|
+    @attributes[name] = 1 if name != 'id' && name != 'created_at' && name != 'updated_at'
+  end
 end
 
 When('I try to submit the form') do
@@ -42,4 +28,46 @@ end
 Then('I do get redirected to the analysis presentation page') do
   expect(page).to have_current_path(survey_response_url(SurveyResponse.last))
   expect(page).to have_content('Survey response was successfully created')
+end
+
+# Data Model Design Steps
+Given('I have a set of invalid attributes') do
+  @survey_profiles_attributes = {}
+  SurveyProfile.column_names.each do |name|
+    @survey_profiles_attributes[name] = nil if name != 'id' && name != 'created_at' && name != 'updated_at'
+  end
+  @survey_responses_attributes = {}
+  SurveyResponse.column_names.each do |name|
+    @survey_responses_attributes[name] = nil if name != 'id' && name != 'created_at' && name != 'updated_at'
+  end
+end
+
+When('I try to create model instances') do
+  p @survey_profiles_attributes
+  p @survey_responses_attributes
+  SurveyProfile.create(@survey_profiles_attributes)
+  SurveyResponse.create(@survey_responses_attributes)
+end
+
+Given('I have a set of valid attributes') do
+  @survey_profiles_attributes = {}
+  SurveyProfile.column_names.each do |name|
+    @survey_profiles_attributes[name] = 10 if name != 'id' && name != 'created_at' && name != 'updated_at'
+  end
+  @survey_responses_attributes = {}
+  SurveyResponse.column_names.each do |name|
+    @survey_responses_attributes[name] = 10 if name != 'id' && name != 'created_at' && name != 'updated_at'
+  end
+end
+
+Then('the model was not created') do
+  expect(SurveyProfile.last.user_id).not_to be_nil
+  expect(SurveyResponse.last.user_id).not_to be_nil
+end
+
+Then('the model was created') do
+  p SurveyProfile.last
+  p SurveyResponse.last
+  expect(SurveyProfile.last.user_id).to eq(10)
+  expect(SurveyResponse.last.user_id).to eq(10)
 end
