0a3f8de5101c00d2d27b376af56777f85f3c9bf9
jacobtoddmathes <thejacobm1@tamu.edu>
2024-02-16 13:06:18 -0600

    all implemented rspec tests passing

--- rails_root/spec/helpers/survey_profiles_helper_spec.rb
+++ rails_root/spec/helpers/survey_profiles_helper_spec.rb
@@ -13,5 +13,37 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe SurveyProfilesHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:survey_profile) do
+    SurveyProfile.create!(
+      user_id: 1,
+      first_name: 'John',
+      last_name: 'Doe',
+      campus_name: 'Campus',
+      district_name: 'District'
+    )
+  end
+
+  describe '#full_name' do
+    it 'returns the full name of a survey profile' do
+      expect(helper.full_name(survey_profile)).to eq('John Doe')
+    end
+  end
+
+  describe '#campus_name' do
+    it 'returns the campus name of a survey profile' do
+      expect(helper.campus_name(survey_profile)).to eq('Campus')
+    end
+  end
+
+  describe '#district_name' do
+    it 'returns the district name of a survey profile' do
+      expect(helper.district_name(survey_profile)).to eq('District')
+    end
+  end
+
+  describe '#get_survey_profile_id' do
+    it 'returns the id of a survey profile' do
+      expect(helper.get_survey_profile_id(survey_profile.user_id)).to eq(survey_profile.id)
+    end
+  end
 end
