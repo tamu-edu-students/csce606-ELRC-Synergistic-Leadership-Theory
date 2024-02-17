c8db8e8e31a38cfba759c68e78f7c1f51295605b
61315 <studio61315@gmail.com>
2024-02-16 21:20:36 -0600

    fix: nav items missing a binding

--- rails_root/spec/views/home/index.html.erb_spec.rb
+++ rails_root/spec/views/home/index.html.erb_spec.rb
@@ -6,6 +6,6 @@ RSpec.describe 'home/index.html.erb', type: :view do
   it 'displays the welcome message' do
     render
 
-    expect(rendered).to have_text('welcome to My Rails App')
+    expect(rendered).to have_text("welcome to Our Rails App")
   end
 end
