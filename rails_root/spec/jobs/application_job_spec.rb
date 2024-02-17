a61910126c503962700c47abffa11aabb151780e
jacobtoddmathes <thejacobm1@tamu.edu>
2024-02-16 22:30:54 -0600

    fixing job test

--- rails_root/spec/jobs/application_job_spec.rb
+++ rails_root/spec/jobs/application_job_spec.rb
@@ -1,11 +1,11 @@
 # require 'rails_helper'
 
-# RSpec.describe ApplicationJob, type: :job do
-#   describe '#perform' do
-#     it 'performs the job' do
-#       expect do
-#         described_class.perform_later
-#       end.to have_enqueued_job(described_class)
-#     end
-#   end
-# end
+RSpec.describe ApplicationJob, type: :job do
+  describe '#perform' do
+    it 'performs the job' do
+      expect do
+        described_class.perform_later
+      end.to have_enqueued_job(described_class)
+    end
+  end
+end
