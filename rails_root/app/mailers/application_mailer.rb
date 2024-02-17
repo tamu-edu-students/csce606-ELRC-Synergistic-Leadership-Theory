adcf758f45bf5bc14335c7075d34bf89e465d0ca
jacobtoddmathes <thejacobm1@tamu.edu>
2024-02-16 22:07:11 -0600

    improved test coverage rating by removing unused logic and adding more tests

--- rails_root/app/mailers/application_mailer.rb
+++ rails_root/app/mailers/application_mailer.rb
@@ -1,9 +1,8 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-# rubocop:disable Style/Documentation
+# class ApplicationMailer < ActionMailer::Base
+#   default from: 'from@example.com'
+#   layout 'mailer'
+# end
+# # rubocop:enable Style/Documentation
 
-class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
-  layout 'mailer'
-end
-# rubocop:enable Style/Documentation
--- rails_root/spec/requests/about_spec.rb
+++ rails_root/spec/requests/about_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Abouts", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Abouts', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/about'
+      expect(response).to have_http_status(:success)
+    end
   end
 end
