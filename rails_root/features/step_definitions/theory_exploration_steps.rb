512df74b9b45c2df04739bcf71a88a2deff16088
Andres Santiago <61444430+Andres-L-Santiago@users.noreply.github.com>
2024-02-29 22:27:52 -0600

    Refined cucumber test suite

---
+++ rails_root/features/step_definitions/theory_exploration_steps.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+When('I visit about page') do
+  visit about_path
+end
+
+Then('I can read about theory information') do
+  expect(page).to have_content('The Four Factors and the Tetrahedron')
+end
