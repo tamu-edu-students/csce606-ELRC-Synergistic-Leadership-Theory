055e7db66837654cc524830dad7c997658f7c94b
61315 <studio61315@gmail.com>
2024-03-08 21:06:58 -0600

    feat: add invitation rspec

---
+++ rails_root/spec/views/invitations/show.html.erb_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'invitations/show', type: :view do
+  before do
+    profile = SurveyProfile.create!(user_id: 1, first_name: 'John', last_name: 'Doe', campus_name: 'Campus', district_name: 'District')
+    survey_response = SurveyResponse.create!(share_code: 'SHARECODE', profile_id: profile.id)
+    @invitation = assign(:invitation, Invitation.create!(survey_response:, created_by: profile, visited: false, last_sent: Time.now))
+    render
+  end
+
+  it 'displays the inviter name' do
+    expect(rendered).to match(@invitation.created_by.first_name)
+    expect(rendered).to match(@invitation.created_by.last_name)
+  end
+
+  it 'displays a button to take the survey' do
+    expect(rendered).to have_selector("form[action=\"/survey_responses/#{@invitation.survey_response.id}\"]")
+  end
+end
