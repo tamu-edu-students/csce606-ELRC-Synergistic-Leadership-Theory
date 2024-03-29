ceeb63102d738ce28e62c1456f87153779be53e3
Luca Lee <kartg0019246@gmail.com>
2024-03-29 12:26:21 -0500

    add history responses and hide nav bar

--- rails_root/app/controllers/home_controller.rb
+++ rails_root/app/controllers/home_controller.rb
@@ -2,5 +2,8 @@
 
 # Controller for the home page
 class HomeController < ApplicationController
-  def index; end
+  def index
+    @survey_responses = SurveyResponse.where(profile_id: 2) # FIXME: use code below when other part works
+    # @survey_responses = SurveyResponse.where(profile_id: session[:userinfo]["sub"])
+  end
 end
