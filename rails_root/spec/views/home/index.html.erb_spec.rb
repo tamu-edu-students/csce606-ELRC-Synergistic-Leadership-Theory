ecfdf17b6039d9961385e3b68ff84f1ed6398abe
Luca Lee <kartg0019246@gmail.com>
2024-03-29 16:51:52 -0500

    add rspec test

--- rails_root/spec/views/home/index.html.erb_spec.rb
+++ rails_root/spec/views/home/index.html.erb_spec.rb
@@ -2,10 +2,19 @@
 
 require 'rails_helper'
 
-RSpec.describe 'home/index.html.erb', type: :view do
-  it 'displays the welcome message' do
-    render
 
-    expect(rendered).to have_text('Welcome to Our Rails App')
+RSpec.describe 'home/index.html.erb', type: :view do
+  it 'displays the please login message' do
+    render
+    expect(rendered).to have_text('You are not logged in. Please login.')
   end
 end
+
+RSpec.describe 'home/index.html.erb', type: :view do
+  it 'displays the welcome message with name' do
+    session[:userinfo] = {"name" => "Peter", "sub" => "testestest"}
+    render
+    expect(rendered).to have_text('Peter - Welcome to Our Rails App')
+  end
+end
+
