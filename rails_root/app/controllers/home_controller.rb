705ef18f7d22f604ccc81c5e2d5c93f354f5bd15
Luca Lee <kartg0019246@gmail.com>
2024-03-29 19:01:36 -0500

    fix rspec

--- rails_root/app/controllers/home_controller.rb
+++ rails_root/app/controllers/home_controller.rb
@@ -4,6 +4,7 @@
 class HomeController < ApplicationController
   def index
     @survey_responses = SurveyResponse.where(profile_id: 2) # FIXME: use code below when other part works
+    # @survey_profile = SurveyProfile.find_by(user_id: session[:userinfo]["sub"])
     # @survey_responses = SurveyResponse.where(profile_id: session[:userinfo]["sub"])
   end
 end
