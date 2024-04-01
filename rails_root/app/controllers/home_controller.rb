beb69e33a16bf6d11155f1e8e0599ff4a8a2f39c
Luca Lee <kartg0019246@gmail.com>
2024-04-01 17:23:16 -0500

    add profile on home page and fix history responses

--- rails_root/app/controllers/home_controller.rb
+++ rails_root/app/controllers/home_controller.rb
@@ -4,7 +4,9 @@
 class HomeController < ApplicationController
   def index
     @survey_responses = SurveyResponse.where(profile_id: 2) # FIXME: use code below when other part works
-    # @survey_profile = SurveyProfile.find_by(user_id: session[:userinfo]["sub"])
-    # @survey_responses = SurveyResponse.where(profile_id: session[:userinfo]["sub"])
+    if !session[:userinfo].nil?
+      @survey_profile = SurveyProfile.find_by(user_id: session[:userinfo]["sub"])
+      @survey_responses = SurveyResponse.where(profile_id: @survey_profile.id)
+    end
   end
 end
