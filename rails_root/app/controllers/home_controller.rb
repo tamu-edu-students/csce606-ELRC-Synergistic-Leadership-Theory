653ea52afe9f663629eaca87285dec687664dc27
LucaLee <88934902+LucaLee207@users.noreply.github.com>
2024-04-10 06:09:49 +0800

    Fix unify feature (#67)
    * add profile on home page and fix history responses
    
    * fix rspec
    
    * changing logic to pass tests
    
    * removing logs
    
    ---------
    
    Co-authored-by: jacobtoddmathes <thejacobm1@tamu.edu>

--- rails_root/app/controllers/home_controller.rb
+++ rails_root/app/controllers/home_controller.rb
@@ -3,8 +3,11 @@
 # Controller for the home page
 class HomeController < ApplicationController
   def index
-    @survey_responses = SurveyResponse.where(profile_id: 2) # FIXME: use code below when other part works
-    # @survey_profile = SurveyProfile.find_by(user_id: session[:userinfo]["sub"])
-    # @survey_responses = SurveyResponse.where(profile_id: session[:userinfo]["sub"])
+    return if session[:userinfo].nil?
+
+    @survey_profile = SurveyProfile.find_by(user_id: session[:userinfo]['sub'])
+    return if @survey_profile.nil?
+
+    @survey_responses = SurveyResponse.where(profile_id: @survey_profile.id)
   end
 end
