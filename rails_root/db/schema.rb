32e4cf11a2610cf94a0e502b7118f89d7b90c448
Andres Santiago <AndyLSantiago20@gmail.com>
2024-04-09 16:34:03 -0500

    Add rails credentials

--- rails_root/db/schema.rb
+++ rails_root/db/schema.rb
@@ -24,6 +24,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_30_011719) do
     t.index ["parent_response_id"], name: "index_invitations_on_parent_response_id"
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "survey_answers", force: :cascade do |t|
     t.integer "choice", null: false
     t.integer "question_id", null: false
