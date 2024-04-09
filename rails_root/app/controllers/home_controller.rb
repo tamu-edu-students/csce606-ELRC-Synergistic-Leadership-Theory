3c88b0b9f719e9c1fea51a9e0a40b5e81d84f516
jacobtoddmathes <thejacobm1@tamu.edu>
2024-04-09 17:06:36 -0500

    changing logic to pass tests

--- rails_root/app/controllers/home_controller.rb
+++ rails_root/app/controllers/home_controller.rb
@@ -3,10 +3,13 @@
 # Controller for the home page
 class HomeController < ApplicationController
   def index
-    @survey_responses = SurveyResponse.where(profile_id: 2) # FIXME: use code below when other part works
-    if !session[:userinfo].nil?
-      @survey_profile = SurveyProfile.find_by(user_id: session[:userinfo]["sub"])
-      @survey_responses = SurveyResponse.where(profile_id: @survey_profile.id)
-    end
+    # @survey_responses = SurveyResponse.where(profile_id: 2) # FIXME: use code below when other part works
+    return if session[:userinfo].nil?
+
+    @survey_profile = SurveyProfile.find_by(user_id: session[:userinfo]['sub'])
+    puts session[:userinfo]['sub'].to_json
+    return if @survey_profile.nil?
+
+    @survey_responses = SurveyResponse.where(profile_id: @survey_profile.id)
   end
 end
