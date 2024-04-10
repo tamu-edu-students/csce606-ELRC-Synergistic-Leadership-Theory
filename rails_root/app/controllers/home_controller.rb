38d338205cc958d86274a52bd4e004a1e6ff75da
Andres Santiago <AndyLSantiago20@gmail.com>
2024-04-10 16:51:45 -0500

    Added redirection to profile creation when user profile is not found for user id

--- rails_root/app/controllers/home_controller.rb
+++ rails_root/app/controllers/home_controller.rb
@@ -5,6 +5,8 @@ class HomeController < ApplicationController
   def index
     return if session[:userinfo].nil?
 
+    redirect_to new_survey_profile_path if SurveyProfile.find_by(user_id: session[:userinfo]['sub']).nil?
+
     @survey_profile = SurveyProfile.find_by(user_id: session[:userinfo]['sub'])
     return if @survey_profile.nil?
 
