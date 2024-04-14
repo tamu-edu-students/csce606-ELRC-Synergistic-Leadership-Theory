3aaad886f745ad6cf25c33c3f1522141ee25050b
61315 <studio61315@gmail.com>
2024-04-14 15:13:57 -0500

    feat: display name in main screen query results

--- rails_root/app/controllers/home_controller.rb
+++ rails_root/app/controllers/home_controller.rb
@@ -10,6 +10,22 @@ class HomeController < ApplicationController
     @survey_profile = SurveyProfile.find_by(user_id: session[:userinfo]['sub'])
     return if @survey_profile.nil?
 
-    @survey_responses = SurveyResponse.where(profile_id: @survey_profile.id)
+    @survey_responses = fetch_survey_responses
+  end
+
+  private
+
+  def fetch_survey_responses
+    SurveyResponse.where(profile_id: @survey_profile.id).map do |response|
+      { response:, invited_by: fetch_invited_by(response) }
+    end
+  end
+
+  def fetch_invited_by(response)
+    invitation = Invitation.find_by(response_id: response.id)
+    parent_response = SurveyResponse.find(invitation.parent_response_id) if invitation
+    profile = SurveyProfile.find(parent_response.profile_id) if parent_response
+    name = "#{profile.first_name} #{profile.last_name}" if profile
+    name || 'N/A'
   end
 end
